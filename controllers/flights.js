26e7a7a1fed3362502226e858cf5b79db21f9fde
ProSweeper <dylan.doucet28@gmail.com>
2022-10-24 15:57:43 +0000

    add create functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,10 +3,22 @@ const Flight = require('../models/flight');
 
 module.exports = {
     new: newFlight,
-
+    create,
 };
 
 function newFlight(req, res) {
     // render the new view for the flights db
     res.render('flights/new', {title: 'Add Flight'});
+}
+
+function create(req, res) {
+    const flight = new Flight(req.body);
+    flight.save(function(err) {
+        // if an error occurs, redirect back to /flights/new
+        if (err) return res.redirect('/flights/new');
+        console.log(flight);
+        // if successful then we redirect since we performed CRUD
+        // remeber to change after index page is added
+        res.redirect('/flights/new')
+    });
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,4 +5,6 @@ var flightsCtrl = require('../controllers/flights');
 
 // GET /flights/new
 router.get('/new', flightsCtrl.new);
+// POST /flights
+router.post('/', flightsCtrl.create);
 module.exports = router;
